cf29ac8d080623b85dbde8ed0aa725d6a9989358
danila <idanila331@gmail.com>
2026-04-06 00:18:53 +0300

    created functionality

---
+++ src/index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const number = String(prompt("Введите число от 0 до 7 включительно(можете вводить в двоичном формате): ")).trim();
+
+if (number.length > 3) {
+  switch (parseInt(number, 2)) {
+    case 0o0:
+      console.log("---");
+      break;
+    case 0o1:
+      console.log("--x");
+      break;
+    case 0o10:
+      console.log("-w-");
+      break;
+    case 0o11:
+      console.log("-wx");
+      break;
+    case 0o100:
+      console.log("r--");
+      break;
+    case 0o101:
+      console.log("r-x");
+      break;
+    case 0o110:
+      console.log("rw-");
+      break;
+    case 0o111:
+      console.log("rwx");
+      break;
+    default:
+      console.log("Вы ввели некорректное число!");
+  }
+} else {
+  switch (parseInt(number, 10)) {
+    case 0:
+      console.log("---");
+      break;
+    case 1:
+      console.log("--x");
+      break;
+    case 2:
+      console.log("-w-");
+      break;
+    case 3:
+      console.log("-wx");
+      break;
+    case 4:
+      console.log("r--");
+      break;
+    case 5:
+      console.log("r-x");
+      break;
+    case 6:
+      console.log("rw-");
+      break;
+    case 7:
+      console.log("rwx");
+      break;
+    default:
+      console.log("Вы ввели некорректное число!");
+  }
+}
