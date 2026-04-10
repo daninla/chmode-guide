485d8e64a6c14f71d0ba2576b174e1386f383f5d
danila <idanila331@gmail.com>
2026-04-10 21:07:43 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,63 +1,44 @@
 "use strict";
 
-const number = String(prompt("Введите число от 0 до 7 включительно(можете вводить в двоичном формате): ")).trim();
+const number = String(
+  prompt(
+    "Введите число от 0 до 7 включительно(можете вводить в двоичном формате): ",
+  ),
+).trim();
 
-if (number.length >= 3) {
-  switch (parseInt(number, 2)) {
-    case 0:
-      console.log("---");
-      break;
-    case 1:
-      console.log("--x");
-      break;
-    case 2:
-      console.log("-w-");
-      break;
-    case 3:
-      console.log("-wx");
-      break;
-    case 4:
-      console.log("r--");
-      break;
-    case 5:
-      console.log("r-x");
-      break;
-    case 6:
-      console.log("rw-");
-      break;
-    case 7:
-      console.log("rwx");
-      break;
-    default:
-      console.log("Вы ввели некорректное число!");
-  }
+if (!number) {
+  return;
+} else if (number.length > 3) {
+  parseInt(number, 2);
 } else {
-  switch (parseInt(number, 10)) {
-    case 0:
-      console.log("---");
-      break;
-    case 1:
-      console.log("--x");
-      break;
-    case 2:
-      console.log("-w-");
-      break;
-    case 3:
-      console.log("-wx");
-      break;
-    case 4:
-      console.log("r--");
-      break;
-    case 5:
-      console.log("r-x");
-      break;
-    case 6:
-      console.log("rw-");
-      break;
-    case 7:
-      console.log("rwx");
-      break;
-    default:
-      console.log("Вы ввели некорректное число!");
-  }
+  parseInt(number, 8);
+}
+
+switch (number) {
+  case 0:
+    console.log("---");
+    break;
+  case 1:
+    console.log("--x");
+    break;
+  case 2:
+    console.log("-w-");
+    break;
+  case 3:
+    console.log("-wx");
+    break;
+  case 4:
+    console.log("r--");
+    break;
+  case 5:
+    console.log("r-x");
+    break;
+  case 6:
+    console.log("rw-");
+    break;
+  case 7:
+    console.log("rwx");
+    break;
+  default:
+    console.log("Вы ввели некорректное число!");
 }
